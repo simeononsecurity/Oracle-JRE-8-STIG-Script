4b6879a8775b3b8a9d4b106f925187409400b3a3
fT5SgrjXeV8khTNVgQmD <4913771+y9pQnhrdix7HkhonyMXiLSo9@users.noreply.github.com>
2020-08-05 21:52:37 -0500

    Update sos-install-java-config.ps1

--- sos-install-java-config.ps1
+++ sos-install-java-config.ps1
@@ -5,35 +5,15 @@
 #- or -
 #<JRE Installation Directory>\lib\deployment.config
 
-$javapath64 = "C:\Program Files\Java"
-$javapath32 = "C:\Program Files (x86)\Java"
-$javadeploy = "C:\temp\java"
+Write-Output "Installing Java Configurations - Please Wait."
+Write-Output "Window will close after install is complete"
 
-echo "Installing Java Configurations - Please Wait."
-echo "Window will close after install is complete"
-If (Test-Path -Path $javadeploy){
-    Write-Host "Java Deployment Properties Installed"
+If (Test-Path -Path "C:\Windows\Sun\Java\Deployment\"){
+    Write-Host "Configs Already Deployed"
 }Else {
-    Write-Host "Installing Java Deployment Properties"
-    Mkdir "C:\temp"
-    Mkdir "C:\temp\java"
-    Copy-Item -Path .\Files\deployment.properties -Destination C:\temp\JAVA\deployment.properties -Force
-    Copy-Item -Path .\Files\exception.sites -Destination C:\temp\JAVA\exception.sites -Force
-}
-
-If (Test-Path -Path $javapath64){
-    for-each ($java64install in $javapath64){
-    Copy-Item -Path .\Files\deployment.config -Destination $javapath64$java64install\lib\deployment.config -Force
-    }
-    Write-Host "java 64-Bit ConfigurationZ Installed"
-}Else {
-    Write-Host "java 64-Bit Is Not Installed"
-}
-If (Test-Path -Path $javapath32){
-    for-each ($java32install in $javapath32){
-    Copy-Item -Path .\Files\deployment.config -Destination $javapath32$java32install\lib\deployment.config -Force
-    }
-    Write-Host "java 32-Bit Configurations Installed"
-}Else {
-    Write-Host "java 32-Bit Is Not Installed"
+    Mkdir "C:\Windows\Sun\Java\Deployment\"
+    Mkdir "C:\temp\JAVA"
+    Copy-Item -Path .\Files\deployment.config -Destination "C:\Windows\Sun\Java\Deployment\" -Force
+    Copy-Item -Path .\Files\deployment.properties -Destination "C:\temp\JAVA\" -Force
+    Copy-Item -Path .\Files\exception.sites -Destination "C:\temp\JAVA\"" -Force
 }
